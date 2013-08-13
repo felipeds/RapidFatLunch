b276de9b7fe1505dcb6719783f7cd3d542a44daa
felipeds <felipeds@gmail.com>
2013-08-12 20:22:55 -0400

    SystemTray working

--- LeagueOfLegendsAutoChat/Program.cs
+++ LeagueOfLegendsAutoChat/Program.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices; 
+using System.Runtime.InteropServices;
+using System.Drawing; 
 
 namespace LeagueOfLegendsAutoChat
 {
-    static class Program
+    public class Program : Form
     {
+
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,7 +22,54 @@ namespace LeagueOfLegendsAutoChat
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Program());
+        }
+        public Program()
+        {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Config", OnConfig);
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "MyTrayApp";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+        }
+        private void OnConfig(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            f.Show();
+        }
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                // Release the icon resource.
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
         }
     }
     
